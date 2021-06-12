ebd6b1efbaa569cd6cb9da2d555b29e49e40e774
judithmg <jmartigo33@gmail.com>
2021-06-12 13:11:19 +0200

    Button styles Button component Added children

--- src/components/Steps/Button.tsx
+++ src/components/Steps/Button.tsx
@@ -1,23 +1,19 @@
-import React from "react";
-import "../../styles/Buttons.scss";
+import React from 'react';
+import '../../styles/Button.scss';
 
 interface Props {
-  className: string;
-  text: string;
-  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+    className: string;
+    onClick?: React.MouseEventHandler<HTMLButtonElement>;
+    children: string;
 }
 
-const Button = ({ className, text, onClick }: Props): JSX.Element => {
-  return (
-    <>
-      <button
-        onClick={onClick}
-        type="button"
-        className={`steps-btn ${className}`}
-      >
-        {text}
-      </button>
-    </>
-  );
+const Button = ({ className, children, onClick }: Props): JSX.Element => {
+    return (
+        <>
+            <button onClick={onClick} type="button" className={`steps-btn ${className}`}>
+                {children}
+            </button>
+        </>
+    );
 };
 export default Button;
